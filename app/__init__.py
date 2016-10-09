94c3ad3964e20aab1523df83fd74a835ecbfad53
kcrong <kcrong@kim82536.pe.kr>
2016-10-09 22:19:38 +0900

    Move manager Initialization Add some space & line

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from flask import Flask
+from flask.ext.script import Manager
 
 app = Flask(__name__)
 
 
 def create_app():
-    from main import main_blueprint
+    from .main import main_blueprint
+
     app.register_blueprint(main_blueprint)
+
     return app
+
+
+manager = Manager(app)
--- manage.py
+++ manage.py
@@ -2,18 +2,13 @@
 # -*- coding: utf-8 -*-
 from flask.ext.script import Manager
 
-# from werkzeug.contrib.fixers import ProxyFix
+from app import app, manager
 
-from app import create_app
-
-
-app = create_app()
-# app.wsgi_app = ProxyFix(app.wsgi_app)
-manager = Manager(app)
 
 @manager.command
 def run():
     app.run()
+
 
 if __name__ == "__main__":
     manager.run()
