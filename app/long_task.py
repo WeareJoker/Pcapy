291970f8ae55015f7dc0863d6d25e6476628ed8e
kcrong <kcrong@kim82536.pe.kr>
2016-10-12 02:27:36 +0900

    Add filesize column

--- app/long_task.py
+++ app/long_task.py
@@ -5,11 +5,10 @@ from .models import *
 
 @celery.task
 def save_pcap(filename, pcap):
-    p = Pcap(filename, pcap.filename)
+    p = Pcap(filename, pcap.filename, pcap.size)
     db.session.add(p)
+    db.session.commit()
 
     pcap.save(os.path.join(PCAP_FILE_PATH, filename))
-
-    db.session.commit()
 
     return True
--- app/models.py
+++ app/models.py
@@ -19,11 +19,13 @@ class Pcap(db.Model):
     id = db.Column(db.INTEGER, primary_key=True)
     fake_filename = db.Column(db.String(50), nullable=False, unique=True)
     real_filename = db.Column(db.String(50), nullable=False)
+    size = db.Column(db.INTEGER, nullable=False)
     is_done = db.Column(db.BOOLEAN, nullable=False, default=False)
 
-    def __init__(self, fake_filename, real_filename):
+    def __init__(self, fake_filename, real_filename, filesize):
         self.fake_filename = fake_filename
         self.real_filename = real_filename
+        self.size = filesize
 
     def __repr__(self):
         return "<Pcap %s>" % self.real_filename
