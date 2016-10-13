2cbfa94f71c17886abeb6485dfe18ee23fe1ed1e
kcrong <kcrong@kim82536.pe.kr>
2016-10-14 06:52:03 +0900

    Add alarm when start analysis

--- app/long_task.py
+++ app/long_task.py
@@ -1,7 +1,12 @@
 from . import celery
+from . import dpkt
 from .config import *
+from .login_manager import current_user
 from .models import *
-from pcapy import open_offline
+from scapy.all import *
+from .analysis_packet import *
+
+from datetime import datetime
 
 
 @celery.task(bind=True)
@@ -13,3 +18,43 @@ def db_test(self):
     print(self)
     print(Pcap.query.all())
     return True
+
+
+packet_info_table = {
+    'DNS': analysis_dns,
+    'HTTP': analysis_http,
+    'ARP': analysis_arp,
+}
+
+
+def analysis_pcap(pcap_path, user):
+    u = current_user(user)
+
+    pcap_filename = os.path.basename(pcap_path)
+
+    db_pcap = Pcap.query.filter_by(fake_filename=pcap_filename).first()
+    db_pcap.when_analysis_started = datetime.now()
+
+    db_pcap.analysis = Analysis()
+
+    # db.session.commit()
+
+    pcap_reader = dpkt.pcap.Reader(open(pcap_path, 'rb'))
+
+    packet_count = 0
+    """
+    for _, buf in pcap_reader:
+        packet_count += 1
+        eth = Ether(buf)
+
+        for proto in packet_info_table.keys():
+            if proto in repr(eth):
+                packet_info_table[proto](eth, db_pcap.analysis)
+    """
+
+    db_pcap.analysis.total_packet = packet_count
+
+    db_pcap.when_analysis_finished = datetime.now()
+    u.alarm.append(Alarm("Success", "Finish Analysis %s" % db_pcap.fake_filename, simple_content="Finish Analysis"))
+
+    db.session.commit()
