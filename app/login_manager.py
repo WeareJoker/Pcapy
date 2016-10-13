69b9f6ae0ce5ba16799486d7970c36eba1c8e5d5
kcrong <kcrong@kim82536.pe.kr>
2016-10-13 23:36:10 +0900

    Add login manager

---
+++ app/login_manager.py
@@ -0,0 +1,50 @@
+from functools import wraps
+from flask import session, redirect, url_for
+from .models import User
+
+
+def login_required(func):
+    @wraps(func)
+    def check_user_login(*args, **kwargs):
+        try:
+            assert session['login']
+        except KeyError:
+            session['login'] = False
+        finally:
+            if session['login'] is True:
+                return func(*args, **kwargs)
+            else:
+                return redirect(url_for('account', msg=2))
+
+    return check_user_login
+
+
+def logout_required(func):
+    @wraps(func)
+    def check_user_logout(*args, **kwargs):
+        try:
+            assert session['login']
+        except KeyError:
+            session['login'] = False
+        finally:
+            if session['login'] is False:
+                return func(*args, **kwargs)
+            else:
+                return redirect(url_for('index'))
+
+    return check_user_logout
+
+
+def login_user(userid):
+    session['login'] = True
+    session['userid'] = userid
+
+
+def logout_user():
+    session['login'] = False
+    session['userid'] = None
+
+
+def current_user():
+    u = User.query.filter_by(userid=session['userid']).first()
+    return u
