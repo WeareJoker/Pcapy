dbb5238be0cfac787b65dfb517844f661bdb0274
kcrong <kcrong@kim82536.pe.kr>
2016-10-18 17:30:25 +0900

    Add Exception when read pcap at long_task

--- app/long_task.py
+++ app/long_task.py
@@ -24,22 +24,26 @@ def analysis_pcap(pcap_path, user):
     db_pcap.analysis = Analysis()
 
     # db.session.commit()
+    try:
+        pcap_reader = dpkt.pcap.Reader(open(pcap_path, 'rb'))
+    except ValueError:
+        db_pcap.is_done = 3
+        db_pcap.error_info = "Invalid file. Is it right pcap file?"
+        u.alarm.append(Alarm("Error", "Error Analysis %s" % db_pcap.fake_filename, simple_content="Error Analysis"))
+    else:
+        packet_count = 0
 
-    pcap_reader = dpkt.pcap.Reader(open(pcap_path, 'rb'))
+        for _, buf in pcap_reader:
+            packet_count += 1
+            eth = Ether(buf)
 
-    packet_count = 0
+            for proto in packet_info_table.keys():
+                if proto in repr(eth):
+                    packet_info_table[proto](eth, db_pcap.analysis)
 
-    for _, buf in pcap_reader:
-        packet_count += 1
-        eth = Ether(buf)
-
-        for proto in packet_info_table.keys():
-            if proto in repr(eth):
-                packet_info_table[proto](eth, db_pcap.analysis)
-
-    db_pcap.analysis.total_packet = packet_count
-    db_pcap.is_done = True
-    db_pcap.when_analysis_finished = datetime.now()
-    u.alarm.append(Alarm("Success", "Finish Analysis %s" % db_pcap.fake_filename, simple_content="Finish Analysis"))
+        db_pcap.analysis.total_packet = packet_count
+        db_pcap.is_done = 1
+        db_pcap.when_analysis_finished = datetime.now()
+        u.alarm.append(Alarm("Success", "Finish Analysis %s" % db_pcap.fake_filename, simple_content="Finish Analysis"))
 
     db.session.commit()
