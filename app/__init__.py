874f8c844da103cf66cf1e4db2f8bcdca7edae09
kcrong <kcrong@kim82536.pe.kr>
2016-10-19 16:29:42 +0900

    Edit __init__ to app factory design

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-app.config.from_pyfile('config.py')
+db = SQLAlchemy()
+celery = Celery()
 
 try:
     import MySQLdb
@@ -35,6 +35,23 @@ def make_celery(app):
     c.Task = ContextTask
     return c
 
+
+def create_app(app):
+    from .main import main_blueprint
+    from .user import user_blueprint
+    from .pcap import pcap_blueprint
+
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(user_blueprint, url_prefix='/'+user_blueprint.name)
+    app.register_blueprint(pcap_blueprint, url_prefix='/'+pcap_blueprint.name)
+
+    app.config.from_pyfile('config.py')
+
+    return app
+
+app = create_app(app)
+
+db.init_app(app)
 
 celery = make_celery(app)
 
