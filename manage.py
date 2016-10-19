45eea8ca5199237c3ddc794461c92cb2d93dc493
kcrong <kcrong@kim82536.pe.kr>
2016-10-19 15:44:42 +0900

    Add db_init for initialize debug user data

--- manage.py
+++ manage.py
@@ -9,5 +9,13 @@ def run():
     app.run(host='0.0.0.0', port=5000, debug=True)
 
 
+@manager.command
+def db_init():
+    from app import db
+    from app.models import User, add_and_commit
+    u = User('asdf', 'asdf')
+    add_and_commit(db.session, u)
+
+
 if __name__ == "__main__":
     manager.run()
