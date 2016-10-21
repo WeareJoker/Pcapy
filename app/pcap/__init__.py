56d42d35b21fbf45703d3692531a03a6a6224b20
kcrong <kcrong@kim82536.pe.kr>
2016-10-21 10:41:57 +0900

    Add template filter get_valid_timestamp func

--- app/pcap/__init__.py
+++ app/pcap/__init__.py
@@ -2,4 +2,16 @@ from flask import Blueprint
 
 pcap_blueprint = Blueprint('pcap', __name__)
 
+
+def get_valid_timestamp(all_pkt_data, pkt_time):
+    for i in range(3):
+        try:
+            return all_pkt_data[i][pkt_time][0].timestamp.isoformat(sep=' ')
+        except KeyError:
+            continue
+    else:
+        return ""
+
+pcap_blueprint.add_app_template_filter(get_valid_timestamp, 'get_valid_timestamp')
+
 from . import views
